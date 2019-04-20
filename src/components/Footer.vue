68c08b080d503cc31bd14e02102c0e18530b44f8
Lima <lmimsra07@gmail.com>
2019-04-20 17:20:24 +0900

    TOPページの雛形を作成

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Made</strong> by <a href="https://www.lmimsra.com">Lima</a>
+        The source code is licensed
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="hero is-dark">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Service Portal
+        </h1>
+        <h2 class="subtitle">
+          All Services
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped></style>
