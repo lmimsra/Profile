cc60b50adbe8cf3a4043c0a70cd25a3077c4dc11
Lima <lmimsra07@gmail.com>
2020-05-01 18:23:43 +0900

    実績の欄を外部に切り出し、共通化

---
+++ src/components/Resume.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="column is-half">
+      <div class="box">
+        <div class="content">
+          <h4 class="title is-5">{{ title }}</h4>
+          {{ about }}<br />
+          ■ 使用技術<br />
+          {{ technology }}<br />
+          ■ 担当<br />
+          {{ role }}<br />
+          ■ 実績<br />
+          {{ performance }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Resume',
+  props: {
+    title: String,
+    about: String,
+    technology: String,
+    role: String,
+    performance: String
+  }
+}
+</script>
+
+<style scoped></style>
